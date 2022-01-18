ad4f4684a8b4157ed20a6badeb10b2c63600a803
Amber Thrall <amber.rose.thrall@gmail.com>
2022-01-17 19:44:01 -0700

    404 page

--- src/components/404.js
+++ src/components/404.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import sadface from '../assets/sadface.png';
 
 class NotFound extends React.Component {
     render() {
         return (
-            <h1>404</h1>
+            <center>
+                <img className="pure-img" src={sadface} alt="Sad Face" />
+                <h1>404</h1>
+                <h2>Page not found</h2>
+            </center>
         );
     }
 }
