6aa836bd4a980d54ee03a2a85c6ae8b511fec00b
Amber Thrall <amber.rose.thrall@gmail.com>
2022-01-17 23:34:33 -0700

    About me

--- src/components/About.js
+++ src/components/About.js
@@ -6,13 +6,22 @@ class About extends React.Component {
             <>
                 <h1>My Name is Amber Thrall</h1>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor felis et nisl rutrum, ac lacinia purus feugiat. Phasellus nisl nulla, scelerisque id turpis a, luctus porttitor quam. Vivamus vitae eleifend justo. Donec gravida at risus quis ultrices. Morbi vitae molestie elit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus aliquet odio erat. Pellentesque risus enim, ultrices id leo id, elementum tincidunt nisi. Sed id gravida erat, sit amet cursus quam. Quisque fringilla laoreet dignissim.
+                    I am a mathematician and computer scientist interested in backend development, programming language theory and algebra. 
+                    I have a bachelors of science in mathematics from Univeristy of Washington.
                 </p>
                 <p>
-Suspendisse rutrum dui non nulla condimentum faucibus. Vestibulum ac convallis felis, non volutpat arcu. Phasellus tempor ultricies nisi ac accumsan. Ut viverra purus aliquam metus consequat, non ullamcorper leo tempus. Vivamus lobortis iaculis massa, quis condimentum dolor eleifend eu. Sed non sodales ante. Quisque venenatis, lorem a rutrum consectetur, enim nisl blandit felis, non facilisis risus libero in purus. Nullam a vehicula mi. Mauris facilisis diam eu diam convallis, id aliquet mi dignissim. Proin tincidunt enim eget arcu fringilla, sit amet molestie ligula rhoncus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec vel varius sapien. Curabitur eu lectus aliquam, interdum arcu ut, fringilla enim. Donec non pellentesque augue.
+                    As a mathematician, I have published two articles relating to eigenvalues:
+                    <ul>
+                        <ol key="1">P. Paparella and A. Thrall. Realizing Suleĭmanova spectra via permutative matrices, II, in press, <i>Linear Algebra Appl.</i>, 2019. DOI: <a href='http://dx.doi.org/10.1016/j.laa.2018.12.030' target="_blank">10.1016/j.laa.2018.12.030</a>.</ol>
+                        <ol key="2">S. Hoover, D. McCormick, P. Paparella, and A. Thrall. On the realizability of the critical points of a realizable list, in press, <i>Linear Algebra Appl.</i>, 2018. DOI: <a href='https://doi.org/10.1016/j.laa.2018.06.024' target="_blank">10.1016/j.laa.2018.06.024</a>.</ol>
+                    </ul>
                 </p>
                 <p>
-Nunc sollicitudin sapien a neque facilisis tempor. Etiam metus nunc, commodo eget efficitur quis, convallis non purus. Mauris convallis nisi sed diam aliquet, mollis sodales velit facilisis. Aliquam erat volutpat. Nam quis est nec neque consectetur luctus a quis neque. Donec placerat hendrerit felis, sit amet lobortis mi. Etiam purus ex, facilisis vel sodales quis, molestie quis lectus. Fusce lorem nisi, molestie nec mollis id, pellentesque non mauris.
+                    This website serves as a portfolio showcasing select projects as well as hosting my writing on math and technology.
+                </p>
+                <p>
+                    I am currently available for hire. If interested please email me at <a href="mailto:amber@thrall.me" target="_blank">amber@thrall.me</a>. 
+                    A copy of my CV is available <a href="/cv.pdf" target="_blank">here</a>.
                 </p>
             </>
         );
